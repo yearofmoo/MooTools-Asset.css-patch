83e22e004b2d070310e2c5facdb75bf605664312
Matias Niemelä <matias@yearofmoo.com>
2011-07-30 22:06:01 -0400

    ported to github

---
+++ Asset.stylesheet.js
@@ -0,0 +1,36 @@
+if(!Browser.ie && !Browser.opera) {
+
+  Asset.stylesheet = function(path,options) {
+
+    options = options || {};
+    options.href = path;
+    options.type = 'text/css';
+    options.rel = 'stylesheet';
+
+    this.id = options.id;
+    if(!this.id) {
+      this.id = options.id = 'Asset.stylesheet-'+(new Date().getTime());
+    }
+    this.element = new Element('link',options).inject(document.getElementsByTagName('head')[0]);
+    var onload = options.onload || function() { };
+
+    var delay = 100;
+    this.checker = (function() {
+      clearInterval(this.timer);
+
+      var sheets = document.styleSheets;
+      for(var i=0;i<sheets.length;i++) {
+        var file = sheets[i];
+        var owner = file.ownerNode ? file.ownerNode : file.owningElement;
+        if(owner && owner.id == this.id) {
+          onload();
+          return;
+        }
+      }
+
+      this.timer = this.checker.delay(delay,this);
+    }.bind(this));
+    this.timer = this.checker.delay(1,this);
+  }
+
+}
