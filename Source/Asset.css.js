16d37f2fdbaf09b55c96a0f9eb6616d7b3d3e2fb
Matias Niemelä <matias@yearofmoo.com>
2012-03-30 10:21:15 -0400

    MooTools 1.2 + 1.3 compatibility working

--- Source/Asset.css.js
+++ Source/Asset.css.js
@@ -1,94 +1,93 @@
-if(!Browser.ie && !Browser.opera) {
-
 (function() {
 
-  var T = typeOf;
-  if(!T) {
-    T = $type;
-  }
+  var isIE = Browser.ie || Browser.Engine.trident;
+  var isOpera = Browser.opera || Browser.Engine.presto;
 
-  var delay = 100;
-  var maxTries = 100;
+  if(!isIE && !isOpera) {
 
-  Asset.css = function(path,options) {
+    var delay = 100;
+    var maxTries = 100;
 
-    if(T(options) == 'function') {
-      options = {
-        onload : options
+    Asset.css = function(path,options) {
+
+      if((typeOf || $type)(options) == 'function') {
+        options = {
+          onload : options
+        }
       }
-    }
-    else {
-      options = options || {};
-    }
+      else {
+        options = options || {};
+      }
 
-    options.href = path;
-    options.type = 'text/css';
-    options.rel = 'stylesheet';
+      options.href = path;
+      options.type = 'text/css';
+      options.rel = 'stylesheet';
 
-    var onload = options.onload || options.onLoad || function() { };
-    var onerror = options.onerror || options.onError || function() { };
-    var isFromOrigin = !path.contains('//');
-    if(!isFromOrigin) {
-      var matches = path.match(/^(.+?:)\/\/(.+?)(:\d+)?(?:\/|\Z)/);
-      var protocol = matches[1].toLowerCase();
-      var hostname = matches[2].toLowerCase();
-      var port = matches[3] || '';
-      isFromOrigin = protocol == window.location.protocol && hostname == window.location.hostname.toLowerCase() && (window.location.port || '') == port;
-    }
+      var onload = options.onload || options.onLoad || function() { };
+      var onerror = options.onerror || options.onError || function() { };
+      var isFromOrigin = !path.contains('//');
+      if(!isFromOrigin) {
+        var matches = path.match(/^(.+?:)\/\/(.+?)(:\d+)?(?:\/|\Z)/);
+        var protocol = matches[1].toLowerCase();
+        var hostname = matches[2].toLowerCase();
+        var port = matches[3] || '';
+        isFromOrigin = protocol == window.location.protocol && hostname == window.location.hostname.toLowerCase() && (window.location.port || '') == port;
+      }
 
-    var id = options.id;
-    if(!id) {
-      id = options.id = 'Asset.css-' + Number(0,1000) + '-' + (new Date().getTime());
-    }
+      var id = options.id;
+      if(!id) {
+        var RAND = 1000;
+        id = options.id = 'Asset.css-' + Number(0,RAND) + '-' + (new Date().getTime());
+      }
 
-    this.element = new Element('link',options).inject(document.getElementsByTagName('head')[0]);
+      this.element = new Element('link',options).inject(document.getElementsByTagName('head')[0]);
 
-    var tries = 0;
-    this.checker = (function() {
-      clearInterval(this.timer);
+      var tries = 0;
+      this.checker = (function() {
+        clearInterval(this.timer);
 
-      var sheets = document.styleSheets;
-      for(var i=0;i<sheets.length;i++) {
-        var file = sheets[i];
-        var owner = file.ownerNode ? file.ownerNode : file.owningElement;
-        if(owner && owner.id == id) {
-          var pass = true;
+        var sheets = document.styleSheets;
+        for(var i=0;i<sheets.length;i++) {
+          var file = sheets[i];
+          var owner = file.ownerNode ? file.ownerNode : file.owningElement;
+          if(owner && owner.id == id) {
+            var pass = true;
 
-          if(isFromOrigin) {
-            var rules = file.cssRules ? 'cssRules' : 'rules';
-            pass = file[rules] && file[rules].length > 0;
-          }
-          else if(!Browser.firefox) {
-            var rules = file.cssRules ? 'cssRules' : 'rules';
-            try {
-              pass = !(file[rules].length == 0);
+            if(isFromOrigin) {
+              var rules = file.cssRules ? 'cssRules' : 'rules';
+              pass = file[rules] && file[rules].length > 0;
             }
-            catch(e) { }
+            else if(!Browser.firefox) {
+              var rules = file.cssRules ? 'cssRules' : 'rules';
+              try {
+                pass = !(file[rules].length == 0);
+              }
+              catch(e) { }
+            }
+
+           if(pass) {
+              onload.apply(this.element);
+            }
+            else {
+              onerror.apply(this.element);
+            }
+            return;
           }
 
-         if(pass) {
-            onload.apply(this.element);
-          }
-          else {
+          tries++;
+          if(tries >= maxTries) {
             onerror.apply(this.element);
+            return;
           }
-          return;
         }
 
-        tries++;
-        if(tries >= maxTries) {
-          onerror.apply(this.element);
-          return;
-        }
-      }
+        this.timer = this.checker.delay(delay,this);
 
-      this.timer = this.checker.delay(delay,this);
+      }.bind(this));
 
-    }.bind(this));
+      this.timer = this.checker.delay(1,this);
+    }
 
-    this.timer = this.checker.delay(1,this);
   }
 
 })();
-
-}
