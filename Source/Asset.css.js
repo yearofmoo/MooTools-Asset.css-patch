ce130555771564bd7ac7ba07c2522f4077c1936b
Matias Niemelä <matias@yearofmoo.com>
2012-03-29 20:43:53 -0400

    Fully working onload and onerror events

---
+++ Source/Asset.css.js
@@ -0,0 +1,76 @@
+if(!Browser.ie && !Browser.opera) {
+
+(function() {
+
+  var T = typeOf;
+  if(!T) {
+    T = $type;
+  }
+
+  var delay = 100;
+  var maxTries = 100;
+
+  Asset.css = function(path,options) {
+
+    if(T(options) == 'function') {
+      options = {
+        onload : options
+      }
+    }
+    else {
+      options = options || {};
+    }
+
+    options.href = path;
+    options.type = 'text/css';
+    options.rel = 'stylesheet';
+
+    var onload = options.onload || options.onLoad || function() { };
+    var onerror = options.onerror || options.onError || function() { };
+    var isFromOrigin = !path.contains('://') || path.match(/^.+?:\/\/(.+?)(?:\/|\Z)/)[0].toLowerCase() == window.location.hostname.toLowerCase();
+
+    var id = options.id;
+    if(!id) {
+      id = options.id = 'Asset.css-'+(new Date().getTime());
+    }
+
+    this.element = new Element('link',options).inject(document.getElementsByTagName('head')[0]);
+
+    var tries = 0;
+    this.checker = (function() {
+      clearInterval(this.timer);
+
+      var sheets = document.styleSheets;
+      for(var i=0;i<sheets.length;i++) {
+        var file = sheets[i];
+        var owner = file.ownerNode ? file.ownerNode : file.owningElement;
+        if(owner && owner.id == id) {
+          if(isFromOrigin) {
+            var rules = owner.cssRules ? owner.cssRules : owner.rules;
+            if(!owner[rules] || owner[rules].length == 0) {
+              onerror.apply(this.element);
+              return;
+            }
+          }
+
+          onload.apply(this.element);
+          return;
+        }
+
+        tries++;
+        if(tries >= maxTries) {
+          onerror.apply(this.element);
+          return;
+        }
+      }
+
+      this.timer = this.checker.delay(delay,this);
+
+    }.bind(this));
+
+    this.timer = this.checker.delay(1,this);
+  }
+
+})();
+
+}
